6927ce14d49c7f039d89aa3e64d03abc2e0a3ee9
Aleksey Trubitsyn <aleksei.trubitsyn@gmail.com>
2025-10-29 19:38:03 +0200

    eslint fix

--- src/pages/home/ui/HomePage/index.tsx
+++ src/pages/home/ui/HomePage/index.tsx
@@ -12,39 +12,57 @@ export function HomePage() {
         <Group gap="lg">
           <div>
             <Icon name="board" size={24} />
-            <Text size="sm" mt="xs">board</Text>
+            <Text size="sm" mt="xs">
+              board
+            </Text>
           </div>
           <div>
             <Icon name="chevron-down" size={24} />
-            <Text size="sm" mt="xs">chevron-down</Text>
+            <Text size="sm" mt="xs">
+              chevron-down
+            </Text>
           </div>
           <div>
             <Icon name="cross" size={24} />
-            <Text size="sm" mt="xs">cross</Text>
+            <Text size="sm" mt="xs">
+              cross
+            </Text>
           </div>
           <div>
             <Icon name="eye" size={24} />
-            <Text size="sm" mt="xs">eye</Text>
+            <Text size="sm" mt="xs">
+              eye
+            </Text>
           </div>
           <div>
             <Icon name="eye-slash" size={24} />
-            <Text size="sm" mt="xs">eye-slash</Text>
+            <Text size="sm" mt="xs">
+              eye-slash
+            </Text>
           </div>
           <div>
             <Icon name="logo" size={24} />
-            <Text size="sm" mt="xs">logo</Text>
+            <Text size="sm" mt="xs">
+              logo
+            </Text>
           </div>
           <div>
             <Icon name="moon" size={24} />
-            <Text size="sm" mt="xs">moon</Text>
+            <Text size="sm" mt="xs">
+              moon
+            </Text>
           </div>
           <div>
             <Icon name="show-more" size={24} />
-            <Text size="sm" mt="xs">show-more</Text>
+            <Text size="sm" mt="xs">
+              show-more
+            </Text>
           </div>
           <div>
             <Icon name="sun" size={24} />
-            <Text size="sm" mt="xs">sun</Text>
+            <Text size="sm" mt="xs">
+              sun
+            </Text>
           </div>
         </Group>
       </Stack>
